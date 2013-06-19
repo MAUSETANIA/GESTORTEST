9bf9aa510d8c5ab9f959589823202a05f6cab2f3
TaniaP <taniapr_18@hotmail.com>
2013-06-20 01:21:41 +0200

    Implementacion de las clases AdministradorDeNotas.java, Calificacion.java

--- Aplicacion GestorTest/Editor de Examenes/src/examen/negocio/Calificacion.java
+++ Aplicacion GestorTest/Editor de Examenes/src/examen/negocio/Calificacion.java
@@ -4,6 +4,8 @@
  */
 package examen.negocio;
 
+import java.util.List;
+
 /**
  *
  * @author Tania
@@ -11,28 +13,68 @@ package examen.negocio;
 public class Calificacion {
     
     public static int devolverPuntaje(Pregunta pregunta){
-        return 0;
+        List<Respuesta> respuestas= pregunta.getRespuestas();
+
+        if(pregunta.getTipo()==1||pregunta.getTipo()==2){
+            for(int i=0;i<respuestas.size();i++){
+                if(respuestas.get(i).isCorrecto()!=respuestas.get(i).isMarcado()){
+                return 0;
+            }
+         }
+
+        }else{
+              for(int i=0;i<respuestas.size();i++){
+                if(!respuestas.get(i).getCompletado().equals(respuestas.get(i).getRespuesta())){
+                    return 0;
+                }
+            }
+        }
+        return (int)pregunta.getPuntos();
         
     }
     
     
     public static int notaExamen(Examen examen){
-        return 0;
+          List<Pregunta> preguntas= examen.getPreguntas();
+        int suma=0;
+        for(int i=0;i<preguntas.size();i++){
+            suma= suma+ devolverPuntaje(preguntas.get(i));
+        }
+        return suma;
         
     }
     
     public static int totalExamen(Examen examen){
-        return 0;
+         List<Pregunta> preguntas= examen.getPreguntas();
+        int suma=0;
+        for(int i=0;i<preguntas.size();i++){
+            suma= suma+ (int)preguntas.get(i).getPuntos();
+        }
+        return suma;
         
     }
     
      public static int cantidadSimplesExamen(Examen examen){
-        return 0;
+        List<Pregunta> preguntas= examen.getPreguntas();
+        int c=0;
+        for (int i = 0; i < preguntas.size(); i++) {
+            if(preguntas.get(i).getTipo()==1){
+                c++;
+            }
+        }
+        return  c;
          
      }
      
      public static int cantidadMultiplesExamen(Examen examen){
-        return 0;
+        List<Pregunta> preguntas= examen.getPreguntas();
+        int c=0;
+        for (int i = 0; i < preguntas.size(); i++) {
+            if(preguntas.get(i).getTipo()==2){
+                c++;
+            }
+        }
+        return  c;    
          
      }
     
