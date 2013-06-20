e7c7f37bea73ae88d6b62f3c401a4a420734c3e2
LorenaVA <lorena-v-a@hotmail.com>
2013-06-20 01:23:56 -0400

    Desarrollo del Proyecto v1.1

---
+++ Aplicacion GestorTest/Editor de Examenes/src/runner/MainRunner.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package runner;
+
+import examen.presentacion.disenador.FormPrincipal;
+
+/**
+ *
+ * @author Lorena
+ */
+public class MainRunner {
+    
+    public static void main(String[] args){
+        new FormPrincipal().setVisible(true);          
+
+    }
+}
