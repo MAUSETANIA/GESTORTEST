0b02356ad66dddb042fa48176fbd886e5134de99
Flavia CC <cc_flavia_u@hotmail.com>
2013-06-21 06:18:46 -0600

    INTEGRACION DEL GESTOR TEST EDITOR DE EXAMENES

--- Aplicacion GestorTest/Editor de Examenes/src/runner/MainRunner.java
+++ Aplicacion GestorTest/Editor de Examenes/src/runner/MainRunner.java
@@ -4,16 +4,70 @@
  */
 package runner;
 
+import com.jtattoo.plaf.AbstractLookAndFeel;
+import com.jtattoo.plaf.acryl.AcrylLookAndFeel;
+import com.jtattoo.plaf.aero.AeroLookAndFeel;
+import com.jtattoo.plaf.aluminium.AluminiumLookAndFeel;
+import com.jtattoo.plaf.bernstein.BernsteinLookAndFeel;
+import com.jtattoo.plaf.fast.FastLookAndFeel;
+import com.jtattoo.plaf.hifi.HiFiLookAndFeel;
+import com.jtattoo.plaf.luna.LunaLookAndFeel;
+import com.jtattoo.plaf.mcwin.McWinLookAndFeel;
+import com.jtattoo.plaf.mint.MintLookAndFeel;
+import com.jtattoo.plaf.noire.NoireLookAndFeel;
+import com.jtattoo.plaf.smart.SmartLookAndFeel;
+import com.sun.java.swing.plaf.motif.MotifLookAndFeel;
+import com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel;
 import examen.presentacion.disenador.FormPrincipal;
 
+import java.io.File;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.LookAndFeel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.multi.MultiLookAndFeel;
+import org.jvnet.substance.SubstanceLegacyDefaultLookAndFeel;
+import org.jvnet.substance.SubstanceLookAndFeel;
+import org.jvnet.substance.skin.SubstanceAutumnLookAndFeel;
+import org.jvnet.substance.skin.SubstanceBusinessBlackSteelLookAndFeel;
+import org.jvnet.substance.skin.SubstanceBusinessBlueSteelLookAndFeel;
+import org.jvnet.substance.skin.SubstanceChallengerDeepLookAndFeel;
+import org.jvnet.substance.skin.SubstanceCremeCoffeeLookAndFeel;
+import org.jvnet.substance.skin.SubstanceCremeLookAndFeel;
+import org.jvnet.substance.skin.SubstanceEmeraldDuskLookAndFeel;
+import org.jvnet.substance.skin.SubstanceMagmaLookAndFeel;
+import org.jvnet.substance.skin.SubstanceMistAquaLookAndFeel;
+import org.jvnet.substance.skin.SubstanceMistSilverLookAndFeel;
+import org.jvnet.substance.skin.SubstanceModerateLookAndFeel;
+import org.jvnet.substance.skin.SubstanceNebulaBrickWallLookAndFeel;
+import org.jvnet.substance.skin.SubstanceNebulaLookAndFeel;
+import org.jvnet.substance.skin.SubstanceOfficeBlue2007LookAndFeel;
+import org.jvnet.substance.skin.SubstanceOfficeSilver2007LookAndFeel;
+import org.jvnet.substance.skin.SubstanceRavenGraphiteGlassLookAndFeel;
+import org.jvnet.substance.skin.SubstanceRavenLookAndFeel;
 /**
  *
- * @author Lorena
+ * @author 
  */
 public class MainRunner {
-    
-    public static void main(String[] args){
-        new FormPrincipal().setVisible(true);          
 
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        File file = new File("C:\\GestorTest\\ASIGNATURA");
+        if (! file.exists()){
+            file.mkdir();
+        }               
+
+        UIManager.setLookAndFeel(new BernsteinLookAndFeel());
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new FormPrincipal().setVisible(true);
+            }
+        });
     }
 }
